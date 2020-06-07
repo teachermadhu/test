f4becbdf3762fa4ff6d1d549066c8659ff6cc11a
teachermadhu <64385453+teachermadhu@users.noreply.github.com>
2020-06-07 12:38:07 +0530

    Add files via upload

---
+++ Drager.js
@@ -0,0 +1,28 @@
+class Drager{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 1
+        }
+        this.drag = Constraint.create(options);
+        this.pointB=pointB;
+        World.add(world, this.drag);
+
+    }
+    fly(){
+        this.drag.bodyA =null;
+    }
+    display(){
+        if(this.drag.bodyA){
+        var pointA = this.drag.bodyA.position;
+        var pointB = this.pointB;
+        
+        strokeWeight(4);
+        stroke("turquoise");
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+        }
+    
+}
---
+++ block.js
@@ -0,0 +1,25 @@
+class Block{
+    constructor(x,y,width,height){
+        var options={
+            'isStatic':false,
+            'friction':0.3,
+            'restitution':0.8,
+            'density':1.0
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body);
+    }
+    display(){
+        var pos= this.body.position;
+        var angle=this.body.angle
+        push();
+       translate(pos.x,pos.y)
+       rotate(angle);
+        rectMode(CENTER);
+       // fill("violet");
+        rect(0,0,this.width,this.height);
+        pop();
+    }
+}
